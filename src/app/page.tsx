bea281e285822c195fe9195b6faf6f42fd4a1eaa
OleksandrTrokhymchuk <postavlikepro@gmail.com>
2025-07-20 17:43:23 +0300

    edit fifth paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -9,7 +7,7 @@ export default function Home() {
         <p>Це другий параграф.</p>
         <p>Це третій параграф.</p>
         <p>Це четвертий параграф.</p>
-        <p>Це п'ятий параграф.</p>
+        <p>Це п'ятий параграф!</p>
       </main>
     </>
   );
